8a67c3b42023b2e3bc4ab67537cd4f5e13b851d4
Laith Budairi <LaithB@exalt.ps>
2021-01-07 16:04:44 +0200

    Add Docker Files and docker-compose.yml

--- ratings-data-service/src/main/java/com/exalt/ratingsdataservice/RatingsDataServiceApplication.java
+++ ratings-data-service/src/main/java/com/exalt/ratingsdataservice/RatingsDataServiceApplication.java
@@ -3,9 +3,11 @@ package com.exalt.ratingsdataservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableEurekaClient
+@PropertySource("classpath:application.properties")
 public class RatingsDataServiceApplication {
 
     public static void main(String[] args) {
@@ -13,3 +15,4 @@ public class RatingsDataServiceApplication {
     }
 
 }
+
